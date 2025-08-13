57fc559ae87f9485322f36ad1e5604e8883d60b0
Marutheesh18 <marutheeshkr307@gmail.com>
2025-08-13 23:46:51 +0530

    AutoWiring

---
+++ Spring/Autowiring/src/main/java/org/example/Alien.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.beans.ConstructorProperties;
+
+public class Alien {
+
+    private int age;
+    private Computer com;
+
+
+    public Alien(){
+        System.out.println("Object Created");
+    }
+
+//    @ConstructorProperties({"age", "lap"})
+//    public Alien(int age,Laptop lap){
+//        System.out.println("Parameterised Constructor Called");
+//
+//        this.age=age;
+//        this.lap=lap;
+//    }
+
+
+    public Computer getCom() {
+        return com;
+    }
+
+    public void setCom(Computer lap) {
+        this.com = com;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void code(){
+        System.out.println("Coding");
+        System.out.println(age);
+        com.compile();
+
+    }
+}
