ae0a88d8b23591fe0d0ba64ef46a4b650bafac09
Marutheesh18 <marutheeshkr307@gmail.com>
2025-08-19 18:38:36 +0530

    Java Based Config

---
+++ Java Based Configuration/JavaBasedConfig/src/main/java/org/example/Laptop.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Laptop implements Computer {
+
+//    public Laptop(){
+//        System.out.println("Laptop Object Created");
+//    }
+
+    @Override
+    public void compile(){
+
+        System.out.println("Compiling Using Laptop");
+    }
+}
